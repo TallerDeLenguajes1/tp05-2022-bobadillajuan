177f5dc12b44c611909e4bc422f6d98e8adfc16c
bobadillajuan <juampi348@gmail.com>
2022-05-26 10:26:02 -0300

    EJ01

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TallerDeLenguajes
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Ejercicio 01 -----\n\n\n");
+
+
+            Console.WriteLine("Ingrese un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num > 0){
+            string n = num.ToString();
+            n.ToCharArray();
+            for (int i = n.Length-1; i>=0; i--){
+            Console.Write("{0}",n[i]);
+            }
+            }else{
+                Console.WriteLine("\nNumero invalido.");
+            }
+        } 
+    }
+}
