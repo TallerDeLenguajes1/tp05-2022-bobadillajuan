1c9890feaed661d8c0ce5dc0f60fe0af73f4c46e
bobadillajuan <juampi348@gmail.com>
2022-05-31 10:09:52 -0300

    Primer commit CalculadoraV2

--- Program.cs
+++ Program.cs
@@ -6,20 +6,59 @@ namespace TallerDeLenguajes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----- Ejercicio 01 -----\n\n\n");
+            Console.WriteLine("----- Ejercicio 02 _ Calculadora -----\n\n\n");
 
+            int controlador;
+            double numeroUno, numeroDos, resultado;
+            do
+            {
+            Console.WriteLine("Por favor, selecciones una operación: \n1) Sumar.\n2) Restar.\n3) Multiplicar.\n4) Dividir.\n5) Salir.\n");
+            controlador = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese un numero");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if(num > 0){
-            string n = num.ToString();
-            n.ToCharArray();
-            for (int i = n.Length-1; i>=0; i--){
-            Console.Write("{0}",n[i]);
+            switch (controlador)
+            {
+                case 1:
+                    Console.WriteLine("\nIngrese el primer numero: ");
+                    numeroUno = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nIngrese el segundo numero: ");
+                    numeroDos = Convert.ToDouble(Console.ReadLine());
+                    resultado = numeroUno + numeroDos;
+                    Console.WriteLine("\nResultado: " + resultado + "\n");
+                break;
+                case 2:
+                    Console.WriteLine("\nIngrese el primer numero: ");
+                    numeroUno = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nIngrese el segundo numero: ");
+                    numeroDos = Convert.ToDouble(Console.ReadLine());
+                    resultado = numeroUno - numeroDos;
+                    Console.WriteLine("\nResultado: " + resultado + "\n");
+                break;
+                case 3:
+                    Console.WriteLine("\nIngrese el primer numero: ");
+                    numeroUno = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nIngrese el segundo numero: ");
+                    numeroDos = Convert.ToDouble(Console.ReadLine());
+                    resultado = numeroUno * numeroDos;
+                    Console.WriteLine("\nResultado: " + resultado + "\n");
+                break;
+                case 4:
+                    Console.WriteLine("\nIngrese el primer numero: ");
+                    numeroUno = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nIngrese el segundo numero: ");
+                    numeroDos = Convert.ToDouble(Console.ReadLine());
+                    resultado = numeroUno / numeroDos;
+                    Console.WriteLine("\nResultado: " + resultado + "\n");
+                break;
+                case 5:
+                    Console.WriteLine("\nSalió del programa correctamente");
+                break;
+                default:
+                    Console.WriteLine("\nIngrese la opcion nuevamente: \n");
+                break;
             }
-            }else{
-                Console.WriteLine("\nNumero invalido.");
-            }
+
+            
+            } while (controlador != 5);
         } 
     }
 }
